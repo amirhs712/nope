7a8c65f90d08a6318b8bd29db48ded72749e43cf
amir hossein <amir751020@gmail.com>
2020-10-28 13:45:27 +0330

    Add minMax support to rules.

--- src/Nope.php
+++ src/Nope.php
@@ -18,13 +18,13 @@ use RuntimeException;
  * Class Nope
  * @package Amirhs712\RuleBuilder
  * @method $this accepted() The field under validation must be yes, on, 1, or true. This is useful for validating "Terms of Service" acceptance.
- * @method $this activeUrl() The field under validation must have a valid A or AAAA record according to the dns_get_record PHP function. The hostname of the provided URL is extracted using the parse_url PHP function before being passed to dns_get_record.
+ * @method $this activeUrl($max = null) The field under validation must have a valid A or AAAA record according to the dns_get_record PHP function. The hostname of the provided URL is extracted using the parse_url PHP function before being passed to dns_get_record.
  * @method $this after(string|Carbon $date) The field under validation must be a value after a given date. The dates will be passed into the strtotime PHP function:
  * @method $this afterOrEqual(string|Carbon $date) The field under validation must be a value after or equal to the given date. For more information, see the after rule.
- * @method $this alpha() The field under validation must be entirely alphabetic characters.
- * @method $this alphaDash() The field under validation may have alpha-numeric characters, as well as dashes and underscores.
- * @method $this alphaNum() The field under validation must be entirely alpha-numeric characters.
- * @method $this array() The field under validation must be a PHP array.
+ * @method $this alpha($min = null, $max = null) The field under validation must be entirely alphabetic characters.
+ * @method $this alphaDash($min = null, $max = null) The field under validation may have alpha-numeric characters, as well as dashes and underscores.
+ * @method $this alphaNum($min = null, $max = null) The field under validation must be entirely alpha-numeric characters.
+ * @method $this array($min = null, $max = null) The field under validation must be a PHP array.
  * @method $this bail() Stop running validation rules after the first validation failure.
  * @method $this before(string|Carbon $date) The field under validation must be a value preceding the given date. The dates will be passed into the PHP strtotime function. In addition, like the after rule, the name of another field under validation may be supplied as the value of date.
  * @method $this beforeOrEqual(string|Carbon $date)
@@ -44,19 +44,19 @@ use RuntimeException;
  * @method $this exists($table, $column = null) The field under validation must exist on a given database table. For more information visit the laravel's documentation.
  * @method $this excludeIf($anotherField, $value) The field under validation will be excluded from the request data returned by the validate and validated methods if the anotherfield field is equal to value.
  * @method $this excludeUnless($anotherField, $value) The field under validation will be excluded from the request data returned by the validate and validated methods unless anotherfield's field is equal to value.
- * @method $this file() The field under validation must be a successfully uploaded file.
+ * @method $this file($max = null) The field under validation must be a successfully uploaded file.
  * @method $this filled() The field under validation must not be empty when it is present.
  * @method $this gt($field) The field under validation must be greater than the given field. The two fields must be of the same type. Strings, numerics, arrays, and files are evaluated using the same conventions as the size rule.
  * @method $this gte($field) The field under validation must be greater than or equal to the given field. The two fields must be of the same type. Strings, numerics, arrays, and files are evaluated using the same conventions as the size rule.
- * @method $this image() The file under validation must be an image (jpeg, png, bmp, gif, svg, or webp)
+ * @method $this image($max = null) The file under validation must be an image (jpeg, png, bmp, gif, svg, or webp)
  * @method $this in(array|In $value) The field under validation must be included in the given list of values.
  * @method $this notIn(array|NotIn $value) The field under validation must not be included in the given list of values.
  * @method $this inArray($anotherField) The field under validation must exist in anotherfield's values.
- * @method $this integer() The field under validation must be an integer. This validation rule does not verify that the input is of the "integer" variable type, only that the input is a string or numeric value that contains an integer.
+ * @method $this integer($min = null, $max = null) The field under validation must be an integer. This validation rule does not verify that the input is of the "integer" variable type, only that the input is a string or numeric value that contains an integer.
  * @method $this ip() The field under validation must be an IP address.
  * @method $this ipv4() The field under validation must be an IPv4 address.
  * @method $this ipv6() The field under validation must be an IPv6 address.
- * @method $this json() The field under validation must be a valid JSON string.
+ * @method $this json($max = null) The field under validation must be a valid JSON string.
  * @method $this lt($field) The field under validation must be less than the given field. The two fields must be of the same type. Strings, numerics, arrays, and files are evaluated using the same conventions as the size rule.
  * @method $this lte($field) The field under validation must be less than or equal to the given field. The two fields must be of the same type. Strings, numerics, arrays, and files are evaluated using the same conventions as the size rule.
  * @method $this max($value) The field under validation must be less than or equal to a maximum value. Strings, numerics, arrays, and files are evaluated in the same fashion as the size rule.
@@ -65,7 +65,7 @@ use RuntimeException;
  * @method $this min($value) The field under validation must have a minimum value. Strings, numerics, arrays, and files are evaluated in the same fashion as the size rule.
  * @method $this notRegex($pattern) The field under validation must not match the given regular expression. Internally, this rule uses the PHP preg_match function. The pattern specified should obey the same formatting required by preg_match and thus also include valid delimiters. Note: When using the regex / not_regex patterns, it may be necessary to specify rules in an array instead of using pipe delimiters, especially if the regular expression contains a pipe character.
  * @method $this nullable() The field under validation may be null. This is particularly useful when validating primitive such as strings and integers that can contain null values.
- * @method $this numeric() The field under validation must be numeric.
+ * @method $this numeric($min = null, $max = null) The field under validation must be numeric.
  * @method $this password($guard = null) The field under validation must match the authenticated user's password. You may specify an authentication guard using the rule's first parameter.
  * @method $this present() The field under validation must be present in the input data but can be empty.
  * @method $this raw(string|\Illuminate\Contracts\Validation\Rule $rule) Add raw validation rules.
@@ -81,10 +81,10 @@ use RuntimeException;
  * @method $this size($value) The field under validation must have a size matching the given value. For string data, value corresponds to the number of characters. For numeric data, value corresponds to a given integer value (the attribute must also have the numeric or integer rule). For an array, size corresponds to the count of the array. For files, size corresponds to the file size in kilobytes.
  * @method $this sometimes()
  * @method $this startsWith() The field under validation must start with one of the given values.
- * @method $this string() The field under validation must be a string. If you would like to allow the field to also be null, you should assign the nullable rule to the field.
+ * @method $this string($min = null, $max = null) The field under validation must be a string. If you would like to allow the field to also be null, you should assign the nullable rule to the field.
  * @method $this timezone() The field under validation must be a valid timezone identifier according to the timezone_identifiers_list PHP function.
  * @method $this unique($table, $column = null) The field under validation must not exist within the given database table.
- * @method $this url() The field under validation must be a valid URL.
+ * @method $this url($min = null, $max = null) The field under validation must be a valid URL.
  * @method $this uuid() The field under validation must be a valid RFC 4122 (version 1, 3, 4, or 5) universally unique identifier (UUID).
  * @method $this when(bool|callable $condition, callable $callback) Add rules conditionally.
  */
@@ -101,6 +101,31 @@ class Nope
         'unique' => Unique::class
     ];
 
+    protected $minMaxRules = [
+        'alpha',
+        'alphaDash',
+        'alphaNum',
+        'array',
+        'integer',
+        'numeric',
+        'string',
+
+        //Only max rules
+        'activeUrl',
+        'file',
+        'image',
+        'json',
+        'url',
+    ];
+
+    protected $maxRules = [
+        'activeUrl',
+        'file',
+        'image',
+        'json',
+        'url',
+    ];
+
     public static function __callStatic($method, $arguments)
     {
         return (new self())->$method(...$arguments);
@@ -109,18 +134,27 @@ class Nope
     public function __call($method, $arguments)
     {
         if (method_exists($this, $localMethod = $method . 'Handler')) {
-            //This method has a custom handler.
             $this->$localMethod(...$arguments);
-        } else if (isset($this->proxyRules[$method])) {
-            $this->handleProxyRule($method, $arguments);
-        } else {
-            $this->apply(Str::snake($method), $this->buildStringArgument($arguments));
+            return $this;
         }
+
+        if (isset($this->proxyRules[$method])) {
+            $this->handleProxyRule($method, $arguments);
+            return $this;
+        }
+
+        if (in_array($method, $this->minMaxRules, true)) {
+            $this->handleMinMaxRule($method, ...$arguments);
+            return $this;
+        }
+
+        $this->apply(Str::snake($method), $this->buildStringArgument($arguments));
+
 
         return $this;
     }
 
-    protected function apply($name, $arguments)
+    protected function apply($name, $arguments = null)
     {
         $this->appliedRules[] = [
             'name' => $name,
@@ -145,6 +179,26 @@ class Nope
             $this->applyProxyRule(...$arguments);
         } else {
             $this->applyProxyRule(Rule::$method(...$arguments));
+        }
+    }
+
+    protected function handleMinMaxRule($method, $first = null, $second = null)
+    {
+        //Apply the rule without arguments
+        $this->apply(Str::snake($method));
+
+        if (in_array($method, $this->maxRules, true)) {
+            //This rule only supports maximum, so the first argument is considered as our max.
+            $this->max($first);
+        } else {
+            //This rule supports both minimum and maximum.
+            if ($first) {
+                $this->min($first);
+            }
+
+            if ($second) {
+                $this->max($second);
+            }
         }
     }
 
--- tests/NopeTest.php
+++ tests/NopeTest.php
@@ -16,23 +16,23 @@ class NopeTest extends \PHPUnit\Framework\TestCase
 {
     public static function testProxyRules()
     {
-        self::assertEquals('in:"1","2","3"', Nope::in([1, 2, 3])->get());
-        self::assertEquals('not_in:"1","2","3"', Nope::notIn([1, 2, 3])->get());
-        self::assertEquals('dimensions:width=1,height=2', Nope::dimensions(['width' => 1, 'height' => 2])->get());
-        self::assertEquals('exists:mysql.users,username', Nope::exists('mysql.users', 'username')->get());
-        self::assertEquals('exists:users,username', Nope::exists('App\Models\User', 'username')->get());
-        self::assertEquals('unique:table,column,"ignoredId",ignoredColumn', Nope::unique
+        self::assertEquals('in:"1","2","3"', nope()->in([1, 2, 3])->get());
+        self::assertEquals('not_in:"1","2","3"', nope()->notIn([1, 2, 3])->get());
+        self::assertEquals('dimensions:width=1,height=2', nope()->dimensions(['width' => 1, 'height' => 2])->get());
+        self::assertEquals('exists:mysql.users,username', nope()->exists('mysql.users', 'username')->get());
+        self::assertEquals('exists:users,username', nope()->exists('App\Models\User', 'username')->get());
+        self::assertEquals('unique:table,column,"ignoredId",ignoredColumn', nope()->unique
         (Rule::unique('table', 'column')->ignore('ignoredId', 'ignoredColumn'))->get());
     }
 
     public static function testProxyRulesWithInstances()
     {
-        self::assertEquals('in:"1","2","3"', Nope::in(Rule::in([1, 2, 3]))->get());
-        self::assertEquals('not_in:"1","2","3"', Nope::notIn(Rule::notIn([1, 2, 3]))->get());
-        self::assertEquals('dimensions:width=1,height=2', Nope::dimensions(Rule::dimensions(['width' => 1])->height(2))->get());
-        self::assertEquals('exists:mysql.users,username', Nope::exists(Rule::exists('mysql.users', 'username'))->get());
-        self::assertEquals('exists:users,username', Nope::exists(Rule::exists('App\Models\User', 'username'))->get());
-        self::assertEquals('unique:table,column,"ignoredId",ignoredColumn', Nope::unique
+        self::assertEquals('in:"1","2","3"', nope()->in(Rule::in([1, 2, 3]))->get());
+        self::assertEquals('not_in:"1","2","3"', nope()->notIn(Rule::notIn([1, 2, 3]))->get());
+        self::assertEquals('dimensions:width=1,height=2', nope()->dimensions(Rule::dimensions(['width' => 1])->height(2))->get());
+        self::assertEquals('exists:mysql.users,username', nope()->exists(Rule::exists('mysql.users', 'username'))->get());
+        self::assertEquals('exists:users,username', nope()->exists(Rule::exists('App\Models\User', 'username'))->get());
+        self::assertEquals('unique:table,column,"ignoredId",ignoredColumn', nope()->unique
         (Rule::unique('table', 'column')->ignore('ignoredId', 'ignoredColumn'))->get());
     }
 
@@ -43,30 +43,35 @@ class NopeTest extends \PHPUnit\Framework\TestCase
             return true;
         };
 
-        self::assertEquals('requiredIf:a,b', Nope::requiredIf('a', 'b')->get());
-        self::assertEquals('required', Nope::requiredIf(true, 'John Doe')->get());
-        self::assertEquals('', Nope::requiredIf(Rule::requiredIf(false))->get());
+        self::assertEquals('requiredIf:a,b', nope()->requiredIf('a', 'b')->get());
+        self::assertEquals('required', nope()->requiredIf(true, 'John Doe')->get());
+        self::assertEquals('', nope()->requiredIf(Rule::requiredIf(false))->get());
 
-        self::assertEquals('required', Nope::requiredIf(Rule::requiredIf($trueCallback))->get());
-        self::assertEquals('required', Nope::requiredIf($trueCallback)->get());
+        self::assertEquals('required', nope()->requiredIf(Rule::requiredIf($trueCallback))->get());
+        self::assertEquals('required', nope()->requiredIf($trueCallback)->get());
     }
 
     public static function testToArrayOutput()
     {
         $object = new stdClass();
-        self::assertEquals(['string', $object, 'sometimes', 'nullable'], Nope::string()->raw($object)->toArray(-2));
+        self::assertEquals(['string', $object, 'sometimes', 'nullable'], nope()->string()->raw($object)->toArray(-2));
     }
 
     public static function testDateRules()
     {
         $date = Carbon::now();
-        self::assertEquals("before:" . $date->toIso8601String(), Nope::before($date)->get());
-        self::assertEquals("before_or_equal:" . $date->toIso8601String(), Nope::beforeOrEqual($date)->get());
-        self::assertEquals("date_equals:" . $date->toIso8601String(), Nope::dateEquals($date)->get());
-        self::assertEquals("date_equals:tomorrow", Nope::dateEquals('tomorrow')->get());
-        self::assertEquals("after:" . $date->toIso8601String(), Nope::after($date)->get());
-        self::assertEquals("after_or_equal:" . $date->toIso8601String(), Nope::afterOrEqual($date)->get());
+        self::assertEquals("before:" . $date->toIso8601String(), nope()->before($date)->get());
+        self::assertEquals("before_or_equal:" . $date->toIso8601String(), nope()->beforeOrEqual($date)->get());
+        self::assertEquals("date_equals:" . $date->toIso8601String(), nope()->dateEquals($date)->get());
+        self::assertEquals("date_equals:tomorrow", nope()->dateEquals('tomorrow')->get());
+        self::assertEquals("after:" . $date->toIso8601String(), nope()->after($date)->get());
+        self::assertEquals("after_or_equal:" . $date->toIso8601String(), nope()->afterOrEqual($date)->get());
     }
 
-
+    public function testMinMaxRules()
+    {
+        self::assertEquals('string|min:1|max:2', nope()->string(1,2)->get());
+        self::assertEquals('alpha_dash|min:1|max:2', nope()->alphaDash(1,2)->get());
+        self::assertEquals('json|max:30', nope()->json(30)->get());
+    }
 }
